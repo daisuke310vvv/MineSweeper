d929fa290cb7fcd4a4edf334d57039c467d89d31
Daisuke Sato <daisuke.sato1991@gmail.com>
2014-12-31 17:13:30 +0900

    Initial Commit

---
+++ MineSweeper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MineSweeper
+//
+//  Created by SatoDaisuke on 12/31/14.
+//  Copyright (c) 2014 SatoDaisuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MineSweeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MineSweeper
+//
+//  Created by SatoDaisuke on 12/31/14.
+//  Copyright (c) 2014 SatoDaisuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
